2d7abafa2c11a5dac6a5018ccf1febea9998bd79
misc72 <193794013+miscellaneous5572@users.noreply.github.com>
2026-03-06 12:11:13 -0500

    fix(logging): `log_level` config parameter now used instead of only `INFO` being applied

--- seadexarr/modules/cli.py
+++ seadexarr/modules/cli.py
@@ -112,33 +112,31 @@ def run_single(
     config = os.path.join(config_dir, "config.yml")
     cache = os.path.join(config_dir, "cache.json")
 
-    logger = setup_logger(log_level="INFO")
-
     if radarr:
         try:
             sdr = SeaDexRadarr(
                 config=config,
                 cache=cache,
-                logger=logger,
+                logger=None,
             )
             sdr.run()
         except Exception:
             tb = traceback.format_exc()
             for line in tb.splitlines():
-                logger.warning(line)
+                sdr.logger.warning(line)
 
     if sonarr:
         try:
             sds = SeaDexSonarr(
                 config=config,
                 cache=cache,
-                logger=logger,
+                logger=None,
             )
             sds.run()
         except Exception:
             tb = traceback.format_exc()
             for line in tb.splitlines():
-                logger.warning(line)
+                sds.logger.warning(line)
 
     return True
 
